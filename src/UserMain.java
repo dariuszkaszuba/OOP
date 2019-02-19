9149ec46cb495cfdaac0bdb1646f3be872979277
Dariusz Kaszuba <dariusz.kaszuba11@gmail.com>
2019-02-19 11:56:05 +0100

    datetime v4

--- src/UserMain.java
+++ src/UserMain.java
@@ -26,8 +26,44 @@ public class UserMain {
                 LocalDate.of(2034,3,01),
                 LocalDateTime.now()
         );
+        uc.addUser(
+                "M",
+                "K",
+                "m",
+                "l",
+                "mk@mk.pl",
+                "450909099",
+                LocalDate.of(2010,10,01),
+                LocalDateTime.now()
+        );
+        uc.addUser(
+                "M",
+                "K",
+                "m",
+                "l",
+                "mk@mk.pl",
+                "450909099",
+                LocalDate.of(1997,01,01),
+                LocalDateTime.now()
+        );
+        uc.addUser(
+                "M",
+                "K",
+                "m",
+                "l",
+                "mk@mk.pl",
+                "450909099",
+                LocalDate.of(1993,04,01),
+                LocalDateTime.now()
+        );
 
-
+        uc.getAllUsers();
+        System.out.println();
+        System.out.println("test date");
+        System.out.println("User pierwszy starszy od drugiego: "+uc.birthDateComparator(0,1));
+        System.out.println("User pierwszy starszy od drugiego: "+uc.birthDateComparator(1,2));
+        System.out.println("Elementy posortowane po dacie ASC");
+        uc.sortByBirthdate();
         uc.getAllUsers();
     }
 }
--- src/controller/UserContoller.java
+++ src/controller/UserContoller.java
@@ -9,16 +9,37 @@ public class UserContoller {
     User users[] = new User[100];
     int index;
 
-    public void addUser(String name, String lastname, String login, String password, String email, String phone, LocalDate birth_datel, LocalDateTime registration_date_time){
-        User u=new User(name, lastname, login, password, email, phone,birth_datel, registration_date_time);
+    public void addUser(String name, String lastname, String login, String password, String email, String phone, LocalDate birth_datel, LocalDateTime registration_date_time) {
+        User u = new User(name, lastname, login, password, email, phone, birth_datel, registration_date_time);
         u.setRegistration_date_time(u.getRegistration_date_time().plusHours(1));
-        users[index]=u;
+        users[index] = u;
         index++;
     }
 
-    public void getAllUsers(){
-        for(int i=0; i<index; i++){
+    public boolean birthDateComparator(User u1, User u2) {
+        return u1.getBirth_datel().isBefore(u2.getBirth_datel());
+    }
+
+    public boolean birthDateComparator(int index1, int index2) {
+        return users[index1].getBirth_datel().isBefore(users[index2].getBirth_datel());
+    }
+
+    public void getAllUsers() {
+        for (int i = 0; i < index; i++) {
             System.out.println(users[i]);
+        }
+    }
+
+    public void sortByBirthdate() {
+        for (int j = 0; j < index; j++) {
+            for (int i = 1; i < index; i++) {
+                if (!birthDateComparator(i - 1, i)) {
+                    User u1 = users[i - 1];
+                    User u2 = users[i];
+                    users[i - 1] = u2;
+                    users[i] = u1;
+                }
+            }
         }
     }
 
